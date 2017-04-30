fdb61f9bfdd75c2e03d9c03454c6696a6040df03
unknown <>
2017-04-30 15:30:30 +0800

    dev_2.0 check in

--- app/src/main/java/com/example/cjw/mytestgit/MainActivity.java
+++ app/src/main/java/com/example/cjw/mytestgit/MainActivity.java
@@ -3,17 +3,28 @@ package com.example.cjw.mytestgit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "dev_2.0 onCreate");
     }
 
     public void go(View view) {
         startActivity(new Intent(this, SecondActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "dev_2.0 onResume");
     }
 }
